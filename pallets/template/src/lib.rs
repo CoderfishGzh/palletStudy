bb15f65fe16a3d1473d2e72ce3e5fc244933dced
向前进 <335247945@qq.com>
2022-03-23 14:53:51 +0800

    shopping test

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -38,6 +38,25 @@ pub mod pallet {
 	// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
 	pub type Something<T> = StorageValue<_, u32>;
 
+	// gzh add 
+    //购物车商品数量,不同id，对应不同的购物车
+    #[pallet::storage]
+    #[pallet::getter(fn totalnums)]
+    pub type TotalNums<T : Config> = StorageMap<_, Blake2_128Concat, T::AccountId, u32>;
+
+    //购物车总价格,不同id，对应不同的购物车
+    #[pallet::storage]
+    #[pallet::getter(fn totalprice)]
+    pub type TotalPrice<T : Config> = StorageMap<_, Blake2_128Concat, T::AccountId, u32>;
+
+    //购物车各个商品的数量，两个key，一个是用户id，一个是商品id，value是数量
+    #[pallet::storage]
+    #[pallet::getter(fn products)]
+    pub type Products<T : Config> = StorageDoubleMap<_, Blake2_128Concat, T::AccountId, Blake2_128Concat, u32, u32>;
+
+
+
+
 	// Pallets use events to inform users when important changes are made.
 	// https://substrate.dev/docs/en/knowledgebase/runtime/events
 	#[pallet::event]
@@ -47,6 +66,14 @@ pub mod pallet {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
 		SomethingStored(u32, T::AccountId),
+
+
+		//gzh add 
+		// user id, product id
+        Addproduct(T::AccountId, u32),
+        // user id, product id
+        Removeproduct(T::AccountId, u32),
+
 	}
 
 	// Errors inform users that something went wrong.
@@ -80,6 +107,116 @@ pub mod pallet {
 			// Return a successful DispatchResultWithPostInfo
 			Ok(())
 		}
+
+
+
+		//gzh add 
+		//给自己账号添加商品
+        #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+        pub fn addproduct(origin: OriginFor<T>, products : u32, price : u32) -> DispatchResult {
+
+            let who = ensure_signed(origin)?;
+
+            //1.判断商品是否存在，存在->num + 1
+            //2.该用户总商品数量+1
+            //3.该用户总开销 + 该商品的价格
+
+            let temp = <Products<T>>::try_get(who.clone(), products);
+            match temp {
+                Ok(t) => {
+                    <Products<T>>::remove(who.clone(), products);
+                    <Products<T>>::insert(who.clone(), products, t + 1);
+                }
+                _ => {
+                    <Products<T>>::insert(who.clone(), products, 1);
+                }
+            }       
+
+            //增加who 商品总数量
+            let temp = <TotalNums<T>>::try_get(who.clone());
+            match temp {
+                Ok(t) => {
+                    <TotalNums<T>>::remove(who.clone());
+                    <TotalNums<T>>::insert(who.clone(), t + 1);
+                }
+                _ => {
+                    <TotalNums<T>>::insert(who.clone(), 1);
+                }
+            }
+
+            //增加who 总金额
+            let temp = <TotalPrice<T>>::try_get(who.clone());
+            match temp {
+                Ok(t) => {
+                    <TotalPrice<T>>::remove(who.clone());
+                    <TotalPrice<T>>::insert(who.clone(), t + price);
+                }
+                _ => {
+                    //用户不存在 直接insert
+                    <TotalPrice<T>>::insert(who.clone(), price);
+                }
+            }
+
+            Self::deposit_event(Event::Addproduct(who.clone(), products));
+            Ok(())
+        }
+
+        //给自己账号删除
+        #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+        pub fn removeproduct(origin: OriginFor<T>, products : u32) -> DispatchResult {
+
+            let who = ensure_signed(origin)?;
+
+            //1.判断商品是否存在
+            //2.商品存在 数量-1， 数量为0直接删除
+            //3.商品不存在，无视操作
+
+            let temp = <Products<T>>::try_get(who.clone(), products);
+            match temp {
+                Ok(num) => {
+                    //user have this product
+                    // num == 1 remove 
+                    if num == 1 {
+                        <Products<T>>::remove(who.clone(), products);
+                    } else {
+                        <Products<T>>::remove(who.clone(), products);
+                        <Products<T>>::insert(who.clone(), products, num - 1);
+                    }
+
+                    //  减少 用户总数量
+                    //  已经存在于products map 里面 直接把总数量减一即可
+                    let products_nums = <TotalNums<T>>::get(who.clone());
+                    match products_nums {
+                        Some(nums) => {
+                            <TotalNums<T>>::remove(who.clone());
+                            <TotalNums<T>>::insert(who.clone(), nums - 1);
+                        }
+                        _ => {
+                            //不做任何事
+                        }
+                    }
+
+
+                    // 减少用户总开销
+                    // TODO
+                    // 增加一个map 存储  商品id ： 价钱
+                }
+                _ => {
+                    //该user 没有这个商品 无视操作
+                }
+            }
+
+
+            //发送 删除商品事件
+            Self::deposit_event(Event::Removeproduct(who.clone(), products));
+
+            Ok(())
+        }
+
+
+
+
+
 
 		/// An example dispatchable that may throw a custom error.
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
